7569e92c0ceb5602bbb247ce43d5ddd80d62f871
EvanBilbrey <Evan.Bilbrey@mt.gov>
2025-01-29 15:01:28 -0700

    Updates to GridMet retrieval and datafile prep. Made datafile compatable with model

--- prep/metdata.py
+++ prep/metdata.py
@@ -7,11 +7,14 @@ import pandas as pd
 import geopandas as gpd
 import xarray as xr
 import numpy as np
-from shapely.geometry import Point
-
 from prep.utils import get_gridmet_cells
-
 from config import GRIDMET_PARAMS
+
+# For get_gridmet_at_polygons function
+import GRIDtools as gt
+from geocube.api.core import make_geocube
+from functools import partial
+from geocube.rasterize import rasterize_image
 
 # TODO - Default date did go to previous day...seems inconsistent with THREDDS,
 #   sometimes it creates a mismatch in the dates and variable series, NEED TO SEE
@@ -158,7 +161,7 @@ def get_gridmet_at_points(in_geom,
                                                                      'units': 'Kelvin'}),
             "max_temp": (['time', 'location'], pd.concat(tmmx, axis=1), {'standard_name': 'Maximum Temperature',
                                                                      'units': 'Kelvin'}),
-            "srad": (['time', 'location'], pd.concat(srad, axis=1), {'standard_name': 'Downward Surface Shortwave Radiation',
+            "solrad": (['time', 'location'], pd.concat(srad, axis=1), {'standard_name': 'Downward Surface Shortwave Radiation',
                                                                      'units': 'W/m^2'}),
             "wind_dir": (['time', 'location'], pd.concat(th, axis=1), {'standard_name': 'Wind Direction',
                                                                      'units': 'Degrees Clockwise from N'}),
@@ -189,6 +192,134 @@ def get_gridmet_at_points(in_geom,
     )
 
     return xds
+
+# Uses geocube package to rasterize and calculate average zonal stats of all parameters except precip
+# Precip parameter is calculated using gridtools package
+
+
+def get_gridmet_at_polygons(in_geom,
+                          gdf_index_col=None,
+                          start = DEFAULT_DATES[0],
+                          end = DEFAULT_DATES[1],
+                          crs = 4326) -> xr.Dataset:
+    """
+    :return: Function takes a list of GridMET data parameters, start date, end date, and a Geopandas GeoDataFrame of
+    polygon geometries and returns a discrete station formatted xarray dataset of mean or area weighted necessary
+    GridMET data to run pydlem for each polygon geometry.
+    :param in_geom: geopandas.GeoDataFrame - contains geometry
+    :param gdf_index_col: str - name of column in GeoDataFrame to use as a unique identifier for each geometry
+    :param start: str "%Y-%m-%d" - Starting date of data extraction period
+    :param end: str "%Y-%m-%d" = Ending date of data extraction period
+    :param crs: int or str - EPSG code for crs, default is 4326
+    :return: a xarray dataset for discrete locations (stations)
+    """
+
+    in_geom = in_geom.astype({gdf_index_col: int})
+    all_polys = in_geom[gdf_index_col].tolist()
+
+    var_list = []
+    for p in tqdm(GRIDMET_PARAMS, desc='Parameters'):
+        bnds = in_geom.total_bounds
+        gmet = GridMet(variable=p, start=start, end=end,
+                       bbox=BBox(bnds[0] - 0.5, bnds[2] + 0.5, bnds[3] + 0.5, bnds[1] - 0.5))
+        gmet = gmet.subset_nc(return_array=True)
+        gmet_input = gmet[list(gmet.data_vars)[0]]
+
+        if p == 'pr':
+            gmet_input = gmet_input / 1000  # convert from mm to meters
+            vol_xds = gt.grid_area_weighted_volume(gmet_input, in_geom, gdf_index_col)
+            # vol_xds = vol_xds.drop('area')
+        else:
+            in_polys = in_geom
+            poly_list = []
+            while all_polys != poly_list:
+                gmet_clipped = gmet_input.rio.write_crs(input_crs=crs).rio.clip(in_polys.geometry.values, in_polys.crs,
+                                                                                all_touched=True)
+                gmet_clipped.name = p
+
+                grid_out = make_geocube(vector_data=in_polys, measurements=[gdf_index_col], like=gmet_clipped,
+                                        rasterize_function=partial(rasterize_image, all_touched=True)).set_coords(
+                    gdf_index_col)
+
+                # make geodataframe to use for rerunning remaining polygons
+                batch = np.unique(
+                    grid_out.coords[gdf_index_col].values[~np.isnan(grid_out.coords[gdf_index_col].values)]).tolist()
+                poly_list.extend(batch)
+                poly_list = sorted(list(map(int, poly_list)), key=all_polys.index)
+                in_polys = in_geom[in_geom[gdf_index_col].isin(list(set(all_polys) - set(poly_list)))]
+
+                for date in range(0, len(gmet_input.time.values)):
+                    gmet_ts = gmet_clipped[date, :, :]
+                    grid_ts = grid_out
+
+                    grid_ts[p] = (grid_out.dims, gmet_ts.values, gmet_ts.attrs, gmet_ts.encoding)
+                    grid_ts = grid_out.drop("spatial_ref").groupby(grid_out[gdf_index_col]).mean()
+                    xda = grid_ts[p]
+                    xda = xda.expand_dims({"time": 1}).assign_coords(time=('time', [gmet_ts.time.values]))
+                    var_list.append(xda)
+
+        xds = xr.merge(var_list)
+
+    lat_df = pd.DataFrame((in_geom.geometry.bounds['miny'] + in_geom.geometry.bounds['maxy']) / 2).set_index(
+        in_geom[gdf_index_col])
+    lat_df = lat_df.reindex(list(xds[gdf_index_col].values.astype(int)))
+
+    lon_df = pd.DataFrame((in_geom.geometry.bounds['minx'] + in_geom.geometry.bounds['maxx']) / 2).set_index(
+        in_geom[gdf_index_col])
+    lon_df = lon_df.reindex(list(xds[gdf_index_col].values.astype(int)))
+
+    loc_elev = pd.DataFrame(
+        py3dep.elevation_bycoords(list(zip(in_geom.geometry.centroid.x, in_geom.geometry.centroid.y)),
+                                  crs=crs)).set_index(in_geom[gdf_index_col])
+    loc_elev = loc_elev.reindex(list(xds[gdf_index_col].values.astype(int)))
+
+    xds = xr.Dataset(
+        {
+            "max_temp": (['time', 'location'], xds["tmmx"].values, {'standard_name': 'Maximum Temperature',
+                                                                    'units': 'Kelvin'}),
+            "min_temp": (['time', 'location'], xds["tmmn"].values, {'standard_name': 'Maximum Temperature',
+                                                                    'units': 'Kelvin'}),
+            "solrad": (
+                ['time', 'location'], xds["srad"].values, {'standard_name': 'Downward Surface Shortwave Radiation',
+                                                           'units': 'W/m^2'}),
+            "wind_dir": (['time', 'location'], xds["th"].values, {'standard_name': 'Wind Direction',
+                                                                  'units': 'Degrees Clockwise from N'}),
+            "wind_vel": (['time', 'location'], xds["vs"].values, {'standard_name': 'Wind Speed',
+                                                                  'units': 'm/s'}),
+            "vpd": (['time', 'location'], xds["vpd"].values, {'standard_name': 'Vapor Pressure Deficit',
+                                                              'units': 'kPa'})
+        },
+        coords={
+            # Keep the order of xds
+            "time": xds['time'].values,
+
+            "location": (['location'], xds[gdf_index_col].values.astype(int), {'long_name': 'location_identifier',
+                                                                               'cf_role': 'timeseries_id'}),
+            "lat": (['location'], list(lat_df.iloc[:, 0]), {'standard_name': 'latitude',
+                                                            'long_name': 'location_latitude',
+                                                            'units': 'degrees',
+                                                            'crs': '4326'}),
+            "lon": (['location'], list(lon_df.iloc[:, 0]), {'standard_name': 'longitude',
+                                                            'long_name': 'location_longitude',
+                                                            'units': 'degrees',
+                                                            'crs': '4326'}),
+            "elev": (['location'], list(loc_elev.iloc[:, 0]), {'standard_name': 'elevation',
+                                                               'long_name': 'location_elevation',
+                                                               'units': 'meters'})
+        },
+        attrs={
+            "featureType": 'timeSeries',
+        }
+    )
+
+    if 'pr' in GRIDMET_PARAMS:
+        output = xr.merge([xds, vol_xds])  # vol_xds reorders to match xds
+    else:
+        output = xds
+
+    return output
+
+
 
 
 def calculate_vpd(Tmin,
